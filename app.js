dd256cb9d211b04c5c5a4f0d4dacee511d2344f0
Kedar V <1365665+kedarv@users.noreply.github.com>
2023-01-05 13:29:40 -0800

    add more functionality, prettier

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-import envSchema from "env-schema";
+import envSchema from 'env-schema';
 import pkg from '@slack/bolt';
+import { Sequelize, DataTypes } from 'sequelize';
 const { App } = pkg;
 
 const schema = {
     type: 'object',
-    required: ['SLACK_SIGNING_SECRET', 'SLACK_BOT_TOKEN'],
+    required: [
+        'SLACK_SIGNING_SECRET',
+        'SLACK_BOT_TOKEN',
+        'SLACK_APP_TOKEN',
+        'DB_PATH',
+    ],
     properties: {
         SLACK_SIGNING_SECRET: {
             type: 'string',
@@ -14,14 +20,49 @@ const schema = {
         },
         SLACK_APP_TOKEN: {
             type: 'string',
-        }
-    }
-}
+        },
+        DB_PATH: {
+            type: 'string',
+        },
+    },
+};
 const config = envSchema({
     schema: schema,
     data: process.env,
     dotenv: true,
-})
+});
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: config.DB_PATH,
+    sync: true,
+    logging: false,
+});
+
+const Mappings = sequelize.define(
+    'Mappings',
+    {
+        id: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+        },
+        channel: DataTypes.STRING,
+        emoji: DataTypes.STRING,
+    },
+    {
+        uniqueKeys: {
+            Items_unique: {
+                fields: ['channel', 'emoji'],
+            },
+        },
+        defaultScope: {
+            attributes: { exclude: ['id'] },
+        },
+    }
+);
+sequelize.sync();
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
@@ -31,28 +72,71 @@ const app = new App({
 });
 
 app.event('reaction_added', async ({ event, client, say }) => {
-    console.log(event.item.channel)
-    try {
-      const result = await client.conversations.history({
-        channel: event.item.channel,
-        latest: event.item.ts,
-        inclusive: true,
-        limit: 1
-      });
-  
-      const message = result.messages[0];
-  
-      console.log('message:', message);
-      await say({text:`got it!`,thread_ts:event.item.ts});
+    const mapping = await Mappings.findOne({
+        where: {
+            channel: event.item.channel,
+            emoji: event.reaction,
+        },
+    });
+    if (mapping) {
+        try {
+            const result = await client.conversations.history({
+                channel: event.item.channel,
+                latest: event.item.ts,
+                inclusive: true,
+                limit: 1,
+            });
+
+            const message = result.messages[0];
+
+            // TODO: write to Gsheetes
+            console.log('message:', message);
+            await say({
+                text: `Recorded :thumbsup:`,
+                thread_ts: event.item.ts,
+            });
+        } catch (e) {
+            console.log('error:', e);
+        }
     }
-    catch(e) {
-      console.log('error:', e);
+});
+
+app.command('/reactji2sheets', async ({ command, ack, respond }) => {
+    await ack();
+    const text = command.text;
+    if (text != 'help' && !text.includes('register')) {
+        await respond("I don't know how to parse that. Use `help` for more.");
+    } else if (text == 'help') {
+        await respond(
+            'reactji2sheets transits Slack messages to Google Sheets. Use /reactji2sheets :emoji: to register the current channel and emoji pairing.'
+        );
+    } else if (text.includes('register')) {
+        const parts = text.split(' ');
+        if (parts.length != 2) {
+            await respond(
+                'Oops! Expected register command to contain an emoji. Sample usage: `/reactji2sheets :wave:`'
+            );
+        } else {
+            try {
+                await Mappings.create({
+                    channel: command.channel_id,
+                    emoji: parts[1].slice(1, -1),
+                });
+                await respond(
+                    `Ok! I've registered ${parts[1]} for the channel <#${command.channel_id}>.`
+                );
+            } catch (error) {
+                if (error.name === 'SequelizeUniqueConstraintError') {
+                    await respond(
+                        `Oops! I already registered ${parts[1]} for <#${command.channel_id}>.`
+                    );
+                }
+            }
+        }
     }
-  });
+});
 
 (async () => {
-    // Start your app
-    await app.start(process.env.PORT || 3000);
-
+    await app.start();
     console.log('⚡️ Bolt app is running!');
 })();
